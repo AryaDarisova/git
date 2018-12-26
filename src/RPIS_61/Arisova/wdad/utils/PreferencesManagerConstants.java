cf269813f127cb1b9a0fad92e54919d9d0faa30b
AryaDarisova <arisova.darya@yandex.ru>
2018-12-26 16:05:14 +0400

    PreferencesManagerAPIChange

---
+++ src/RPIS_61/Arisova/wdad/utils/PreferencesManagerConstants.java
@@ -0,0 +1,10 @@
+package RPIS_61.Arisova.wdad.utils;
+
+public interface PreferencesManagerConstants {
+    final String CREATE_REGISTRY = "appconfig.rmi.server.registry.createregistry";
+    final String REGISTRY_ADDRESS = "appconfig.rmi.server.registry.registryaddress";
+    final String REGISTRY_PORT = "appconfig.rmi.server.registry.registryport";
+    final String POLICY_PATH = "appconfig.rmi.client.policypath";
+    final String USE_CODE_BASE_ONLY = "appconfig.rmi.client.usecodebaseonly";
+    final String CLASS_PROVIDER = "appconfig.rmi.classprovider";
+}
